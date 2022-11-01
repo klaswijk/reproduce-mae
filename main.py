39a808dff0eb5568ee731833e9e1b4393dbc0481
noah <noah_mannberg@hotmail.com>
2022-11-01 16:03:22 +0100

    add cuda support

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ MODEL_PATH = "./cifar_mae.pth"
 
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5
     npimg = img.detach().numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
 
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -29,7 +31,7 @@ def main():
         transform=transform
     )
 
-    trainloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+    trainloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True)
 
     model = MAE(
         image_size=32,
@@ -38,21 +40,21 @@ def main():
         encoder_num_heads=4,
         encoder_hidden_dim=12,
         encoder_mlp_dim=12,
-        decoder_layers=12,
+        decoder_layers=4,
         decoder_num_heads=4,
         decoder_hidden_dim=12,
         decoder_mlp_dim=12,
         mask_ratio=0.5,
-    )
+    ).to(device)
 
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.9)
 
-    for epoch in range(1):
+    for epoch in range(10):
 
         running_loss = 0.0
-        for i, data in enumerate(trainloader):
-            inputs, _ = data
+        for i, (data, _) in enumerate(trainloader):
+            inputs = data.to(device)
 
             optimizer.zero_grad()
             outputs = model(inputs)
@@ -61,15 +63,13 @@ def main():
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 2000 == 1999:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
 
-                imshow(utils.make_grid(torch.vstack([inputs, outputs]), nrow=4))
+        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / i:.3f}')
 
-    print("Finished training")
-    torch.save(model.state_dict(), MODEL_PATH)
-    print("Saved model")
+        imshow(utils.make_grid(torch.vstack([inputs[:4], outputs[:4]]), nrow=4))
+        print(f"Finished epoch: {epoch}")
+        torch.save(model.state_dict(), MODEL_PATH)
+        print("Saved model")
 
 
 if __name__ == "__main__":
