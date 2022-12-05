fb963894fc6c7541fafb214523df61ce22bed58c
Klas Wijk <klas.wijk@gmail.com>
2022-12-05 11:08:34 +0100

    refactor data.py

--- data.py
+++ data.py
@@ -1,12 +1,13 @@
 import os
 import json
+import torch
+import numpy as np
 from collections import namedtuple
 from pandas import read_csv
-import torch
-from torch.utils.data import DataLoader, Subset, Dataset
+from torch.utils.data import DataLoader, Subset, Dataset, random_split
 from torchvision import datasets, transforms
 from PIL import Image
-import numpy as np
+
 
 DatasetInfo = namedtuple(
     "DatasetInfo", ["image_size", "n_classes", "multilabel"])
@@ -18,7 +19,7 @@ info = {
 
 
 class ImageNetteDataset(Dataset):
-    def __init__(self, path, transform=None, target_transform=None, test=False):
+    def __init__(self, path, transform=None, test=False):
         # Should have 10000 train and 3395 val ("test") images
         self.img_labels = read_csv(path + "noisy_imagenette.csv")
         if test:
@@ -27,7 +28,19 @@ class ImageNetteDataset(Dataset):
             self.img_labels = self.img_labels[:10000]
         self.img_dir = path
         self.transform = transform
-        self.target_transform = target_transform
+        targets = {
+            "n01440764": 0,
+            "n02979186": 1,
+            "n03028079": 2,
+            "n03417042": 3,
+            "n03445777": 4,
+            "n02102040": 5,
+            "n03000684": 6,
+            "n03394916": 7,
+            "n03425413": 8,
+            "n03888257": 9,
+        }
+        self.target_transform = transforms.Lambda(targets.get)
 
     def __len__(self):
         return len(self.img_labels)
@@ -46,13 +59,12 @@ class ImageNetteDataset(Dataset):
 
 class CocoMultilabel(Dataset):
 
-    def __init__(self, path, transform=None, target_transform=None, test=False, version="2017"):
+    def __init__(self, path, transform=None, test=False, version="2017"):
         datatype = "val" if test else "train"
         with open(f"{path}/coco/annotations/instances_{datatype}{version}.json", "r") as f:
             instances = json.load(f)
 
         self.transform = transform
-        self.target_transform = target_transform
         self.datapath = f"{path}/coco/{datatype}{version}"
         self.images = [image["file_name"] for image in instances["images"]]
         self.image_ids = [image["id"] for image in instances["images"]]
@@ -78,254 +90,99 @@ class CocoMultilabel(Dataset):
         label = self.labels[self.image_ids[idx]]
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-
         return image, label
 
 
-def coco(train, device, checkpoint, transform):
-    """Returns (dataloader, image_size)"""
-    data_path = checkpoint["data_path"]
-    limit = checkpoint["config"]["data"]["limit"]
-    val_ratio = checkpoint["config"]["data"]["val_ratio"]
-    batch_size = checkpoint["config"]["batch_size"]
-
-    if train:
-        dataset = CocoMultilabel(
-            data_path,
-            transform=transform,
-        )
-
-        idx = np.array(list(range(len(dataset))))
-        np.random.shuffle(idx)
-
-        if limit and limit > -1:
-            dataset = Subset(dataset, idx[: limit])
-            idx = np.array(list(range(len(dataset))))
-            np.random.shuffle(idx)
-
-        valsize = int(val_ratio * len(dataset))
-        valset = Subset(dataset, idx[-valsize:])
-        trainset = Subset(dataset, idx[: -valsize])
-
-        trainloader = DataLoader(
-            trainset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
-        )
-        valloader = DataLoader(
-            valset,
-            batch_size=batch_size,
-            shuffle=False,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
-        )
-        return trainloader, valloader
-    else:
-        dataset = CocoMultilabel(
-            data_path,
-            transform=transform,
-            test=True
-        )
-        testloader = DataLoader(
-            dataset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
-        )
-        return testloader
-
-
-def imagenette(train, device, checkpoint):
-    """Returns (dataloader, image_size)"""
-    data_path = checkpoint["data_path"]
-    limit = checkpoint["config"]["data"]["limit"]
-    val_ratio = checkpoint["config"]["data"]["val_ratio"]
-    batch_size = checkpoint["config"]["batch_size"]
-
-    transform = transforms.Compose([
+def get_transform(dataset_name, transform_type):
+    transform_list = [
+        transforms.RandomCrop(info[dataset_name].image_size),
         transforms.ToTensor(),
-        transforms.RandomCrop((160, 160)),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    ]
+    if transform_type == "finetune":
+        transform_list = [transforms.RandAugment()] + transform_list
+    if dataset_name == "coco":
+        size = int(info[dataset_name].image_size * 1.1)
+        transform_list = [transforms.Resize(
+            size, antialias=True)] + transform_list
+    return transforms.Compose(transform_list)
 
-    targets = {
-        "n01440764": 0,
-        "n02979186": 1,
-        "n03028079": 2,
-        "n03417042": 3,
-        "n03445777": 4,
-        "n02102040": 5,
-        "n03000684": 6,
-        "n03394916": 7,
-        "n03425413": 8,
-        "n03888257": 9,
-    }
 
-    target_transform = transforms.Lambda(targets.get)
+def get_dataloader(dataset_name, train, device, checkpoint, transform_type=None):
+    # Get transform
+    if not train:
+        transform_type = None
+    transform = get_transform(dataset_name, transform_type)
 
-    if train:
+    # Get dataset
+    data_path = checkpoint["data_path"]
+    if dataset_name == "cifar10":
+        dataset = datasets.CIFAR10(
+            root=data_path,
+            train=train,
+            download=True,
+            transform=transform
+        )
+    elif dataset_name == "imagenette":
         dataset = ImageNetteDataset(
             data_path + "/imagenette2-160/",
             transform=transform,
-            target_transform=target_transform
+            test=not train
         )
-
-        idx = np.array(list(range(len(dataset))))
-        np.random.shuffle(idx)
-
-        if limit and limit > -1:
-            dataset = Subset(dataset, idx[: limit])
-            idx = np.array(list(range(len(dataset))))
-            np.random.shuffle(idx)
-
-        valsize = int(val_ratio * len(dataset))
-        valset = Subset(dataset, idx[-valsize:])
-        trainset = Subset(dataset, idx[: -valsize])
-        trainloader = DataLoader(
-            trainset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
-        )
-        valloader = DataLoader(
-            valset,
-            batch_size=batch_size,
-            shuffle=False,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
-        )
-        return trainloader, valloader
-    else:
-        dataset = ImageNetteDataset(
-            data_path + "/imagenette2-160/",
+    elif dataset_name == "coco":
+        dataset = CocoMultilabel(
+            data_path,
             transform=transform,
-            target_transform=target_transform,
-            test=True
+            test=not train
         )
-        testloader = DataLoader(
-            dataset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
-        )
-        return testloader
+    else:
+        raise ValueError(f"Unknown dataset '{dataset_name}'")
 
-
-def cifar(train, device, checkpoint):
-    """Returns (dataloader, image_size)"""
-    data_path = checkpoint["data_path"]
+    # Use a subset of the data for debugging
     limit = checkpoint["config"]["data"]["limit"]
-    val_ratio = checkpoint["config"]["data"]["val_ratio"]
-    batch_size = checkpoint["config"]["batch_size"]
-
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    dataset = datasets.CIFAR10(
-        root=data_path,
-        train=train,
-        download=True,
-        transform=transform
-    )
     if limit and limit > -1:
         dataset = Subset(dataset, range(limit))
 
-    if train:
-        idx = np.array(list(range(len(dataset))))
-        np.random.shuffle(idx)
+    # Return dataloader
+    num_workers = 4
+    pin_memory = str(device) != "cpu"
+    pin_memory_device = str(device) if str(device) != "cpu" else ""
+    batch_size = checkpoint["config"]["batch_size"]
 
+    if train:
+        # Train/val split
+        val_ratio = checkpoint["config"]["data"]["val_ratio"]
         valsize = int(val_ratio * len(dataset))
-        valset = Subset(dataset, idx[-valsize:])
-        trainset = Subset(dataset, idx[: -valsize])
+        trainsize = len(dataset) - valsize
+        trainset, valset = random_split(
+            dataset,
+            lengths=[trainsize, valsize],
+            generator=torch.Generator().manual_seed(42)  # "deterministic"
+        )
         trainloader = DataLoader(
             trainset,
             batch_size=batch_size,
             shuffle=True,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            pin_memory_device=pin_memory_device
         )
         valloader = DataLoader(
             valset,
             batch_size=batch_size,
             shuffle=False,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            pin_memory_device=pin_memory_device
         )
         return trainloader, valloader
     else:
         testloader = DataLoader(
             dataset,
             batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=str(device) != "cpu",
-            pin_memory_device=str(device) if str(device) != "cpu" else ""
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            pin_memory_device=pin_memory_device
         )
         return testloader
-
-
-def get_dataloader(dataset, train, device, checkpoint, transform_type=None):
-    if not train:
-        transform_type = None
-    if transform_type == "finetune":
-        if dataset == "coco":
-            transform = transforms.Compose([
-                transforms.Resize(
-                    int(info["coco"].image_size * 1.1), antialias=True),
-                transforms.RandAugment(),
-                transforms.RandomCrop(info["coco"].image_size),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ])
-        else:
-            transform = transforms.Compose([
-                transforms.RandAugment(),
-                transforms.RandomCrop(info[dataset].image_size),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ])
-    elif transform_type == "pretrain":
-        if dataset == "coco":
-            transform = transforms.Compose([
-                transforms.RandomResizedCrop(
-                    info["coco"].image_size, ratio=(1, 1)),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ])
-        else:
-            transform = transforms.Compose([
-                transforms.RandomCrop(info[dataset].image_size),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ])
-    else:
-        transform = transforms.Compose([
-            transforms.Resize(info[dataset].image_size, antialias=True),
-            transforms.RandomCrop(info[dataset].image_size),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ])
-    if dataset == "cifar10":
-        return cifar(train, device, checkpoint)
-    elif dataset == "imagenette":
-        return imagenette(train, device, checkpoint)
-    elif dataset == "coco":
-        return coco(train, device, checkpoint, transform)
-    else:
-        raise ValueError(f"Unknown dataset '{dataset}'")
